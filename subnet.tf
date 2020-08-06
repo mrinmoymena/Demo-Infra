769d47ad17545483963e8e6ea21deb089d50b0bb
mmena <mmena@tripwire.com>
2020-08-06 18:14:06 +0530

    public ip for subnet

--- subnet.tf
+++ subnet.tf
@@ -4,7 +4,7 @@ resource "aws_subnet" "eks_demo_sn" {
   availability_zone = data.aws_availability_zones.available.names[count.index]
   cidr_block        = "10.0.${count.index}.0/24"
   vpc_id            = aws_vpc.vpc_eks_demo.id
-
+  map_public_ip_on_launch = true
   tags = map(
     "Name", "eks-demo-sn",
     "kubernetes.io/cluster/${var.cluster_name}", "shared",
